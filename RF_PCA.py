9e016382d73f4686b5440e309fab6d8996a99ae4
Andrea <andrea.baiocco01@gmail.com>
2025-04-01 12:53:50 +0200

    Tentativo di PCA

--- RF_PCA.py
+++ RF_PCA.py
@@ -2,9 +2,11 @@ import numpy as np
 import sklearn.ensemble as ens
 import matplotlib.pyplot as plt
 import time as clock
+import pandas as pd
 
 from sklearn.model_selection import KFold
 from scipy.io import loadmat
+from scipy.linalg import eig
 
 start = clock.time()
 
@@ -14,26 +16,57 @@ data = loadmat(path_file)
 
 g0 = data['g__0']
 g1 = data['g__1']
+
+print("Calculating correlation matrix...")
+tot_pattern = np.concatenate((g0, g1), axis=0)
+corr = np.corrcoef(tot_pattern, rowvar=False)
+
+print('Calculating eigenvalues and eigenvectors...')
+e_val, e_vec = eig(corr) #in teoria pi� efficiente con scipy
+e_vec = np.transpose(e_vec)
+
+print('Sorting eigenvalues and eigenvectors...\n')
+sort_index = np.argsort(e_val)[::-1]
+e_val = e_val[sort_index].real()
+e_vec = e_vec[sort_index,:].real()
+
+for i in range(len(e_val[:20])):
+    print('Eigenvalue: {}'.format(e_val[i]))
+    print('Eigenvector: {}\n'.format(e_vec[i]))
+
+e_val_sum = np.sum(e_val)
+
+val_sum = 0
+print('\nOriginal\tPercent\tCumulative')
+for i,val in enumerate(e_val[:20]):
+    val_sum += val
+    print('{}\t{:.2%}\t{.2%}'.format(sort_index[i], val/e_val_sum, val_sum/e_val_sum))
+
+df_eval = pd.DataFrame(data = e_val)
+df_evec = pd.DataFrame(data = e_vec)
+df_eval.to_csv('eigenvalues.csv', sep = ',')
+df_evec.to_csv('eigenvectors.csv', sep = ',')
+
 tot_pattern = np.concatenate((g0[:17], g1[:17]), axis=0)
-int_test_pattern = np.concatenate((g0[18:], g1[18:]), axis=0)
+ext_test_pattern = np.concatenate((g0[18:], g1[18:]), axis=0)
 
 Nneg = len(g0[:, 0])
 Npos = len(g1[:, 0])
 
-Nneg_int = int(len(int_test_pattern[:, 0])/2)
-Npos_int = int(len(int_test_pattern[:, 0])/2)
+Nneg_ext = int(len(ext_test_pattern[:, 0])/2)
+Npos_ext = int(len(ext_test_pattern[:, 0])/2)
 
 label0 = 'NEGATIVI'
 label1 = 'POSITIVI'
 g0_labels = np.full(Nneg, label0)
 g1_labels = np.full(Npos, label1)
 tot_labels = np.concatenate((g0_labels[:17], g1_labels[:17]), axis=0)
-int_test_labels = np.concatenate((g0_labels[18:], g1_labels[18:]), axis=0)
+ext_test_labels = np.concatenate((g0_labels[18:], g1_labels[18:]), axis=0)
 
 # print(len(tot_labels))
 # print(tot_labels)
-# print(len(int_test_labels))
-# print(int_test_labels)
+# print(len(ext_test_labels))
+# print(ext_test_labels)
 
 # Iperparameters
 k = 5
@@ -47,8 +80,8 @@ fold_specificity = []
 kf = KFold(n_splits = k, shuffle = True, random_state = 8)
 indices = kf.split(tot_labels)
 
-vote_pos = np.zeros(len(int_test_labels))
-vote_neg = np.zeros(len(int_test_labels))
+vote_pos = np.zeros(len(ext_test_labels))
+vote_neg = np.zeros(len(ext_test_labels))
 
 for i, (train_index, test_index) in enumerate(indices):
 
@@ -101,10 +134,10 @@ for i, (train_index, test_index) in enumerate(indices):
     fold_sensitivity.append(temp_sensitivity)
     fold_specificity.append(temp_specificity)
 
-    # internal tests: Majority vote
-    int_prediction = model.predict(int_test_pattern)
+    # External tests: Majority vote
+    ext_prediction = model.predict(ext_test_pattern)
 
-    for j, pred in enumerate(int_prediction):
+    for j, pred in enumerate(ext_prediction):
         if pred == label0:
             vote_neg[j] += 1
         if pred == label1:
@@ -133,27 +166,27 @@ print("Sensitivity: {:.2%} +- {:.2%} Rel: {:.2%}".format(sensitivity, sensitivit
 print("Specificity: {:.2%} +- {:.2%} Rel: {:.2%}".format(specificity, specificity_std, spec_rel))
 print("\n")
 
-# Performance of internal Test
+# Performance of External Test
 
-int_accuracy = 0.
-int_sensitivity = 0.
-int_specificity = 0.
+ext_accuracy = 0.
+ext_sensitivity = 0.
+ext_specificity = 0.
 
 print(vote_neg)
 print(vote_pos)
 
-for i, true_label in enumerate(int_test_labels):
+for i, true_label in enumerate(ext_test_labels):
     if vote_neg[i]>=vote_pos[i] and true_label == label0:
-        int_accuracy += 1./(Npos_int+Nneg_int)
-        int_specificity += 1./Nneg_int
+        ext_accuracy += 1./(Npos_ext+Nneg_ext)
+        ext_specificity += 1./Nneg_ext
     if vote_neg[i]<vote_pos[i] and true_label == label1:
-        int_accuracy += 1./(Npos_int+Nneg_int)
-        int_sensitivity += 1./Npos_int
+        ext_accuracy += 1./(Npos_ext+Nneg_ext)
+        ext_sensitivity += 1./Npos_ext
 
-print("Performance of internal test")
-print("Accuracy: {:.2%}".format(int_accuracy))
-print("Sensitivity: {:.2%}".format(int_sensitivity))
-print("Specificity: {:.2%}".format(int_specificity))
+print("Performance of external test")
+print("Accuracy: {:.2%}".format(ext_accuracy))
+print("Sensitivity: {:.2%}".format(ext_sensitivity))
+print("Specificity: {:.2%}".format(ext_specificity))
 print("\n")
 
 print("Tempo:" + str(round((clock.time() - start)/60)) + "'" + str(round((clock.time() - start)%60)) + "''")
