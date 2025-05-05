9d60fd49b7bb8ce52388d620528a2a6d3f94a936
Andrea Baiocco <andrea.baiocco01@gmail.com>
2025-05-05 11:55:57 +0200

    Merged RF_Library versions

--- RF_Library.py
+++ RF_Library.py
@@ -1,17 +1,19 @@
 import numpy as np
 import sklearn.ensemble as ens
-import multiprocessing as mp
 import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
 import matplotlib.table as tab
 import tkinter as tk
 
 from sklearn.model_selection import KFold
 from scipy.optimize import curve_fit
 from scipy.stats import ttest_ind
+from matplotlib import cm, colors
 
 def RF_binary_kfold(n_trees, k, patterns, labels, label0, label1, ext_patt = None, ext_lab = None):
     
+    models = []
+
+    # Metrics of single folds
     fold_accuracy = []
     fold_sensitivity = []
     fold_specificity = []
@@ -53,7 +55,9 @@ def RF_binary_kfold(n_trees, k, patterns, labels, label0, label1, ext_patt = Non
                                                bootstrap=True)
 
         model.fit(train_pattern, train_labels)
+        models.append(model)
 
+        # Internal test
         test_prediction = model.predict(test_pattern)
 
         temp_Npos = 0
@@ -82,6 +86,7 @@ def RF_binary_kfold(n_trees, k, patterns, labels, label0, label1, ext_patt = Non
         fold_sensitivity.append(temp_sensitivity)
         fold_specificity.append(temp_specificity)
 
+        # External test: Majority vote counting
         if ext_lab != None:
             ext_pred = model.predict(ext_patt)
 
@@ -91,18 +96,83 @@ def RF_binary_kfold(n_trees, k, patterns, labels, label0, label1, ext_patt = Non
                 if pred == label1:
                     vote_1_ext[j] += 1
 
+    # Performance of External Test: Best in Training
+    # Best Accuracy
+    bacc_model = models[np.argmax(fold_accuracy)]
+    bacc_accuracy = 0.
+    bacc_sensitivity = 0.
+    bacc_specificity = 0.
+
+    if ext_lab != None:
+        bacc_predict = bacc_model.predict(ext_patt)
+
+        for j, pred in enumerate(bacc_predict):
+            if pred == label1 and ext_lab[j] == label1:
+                bacc_accuracy += 1./(N1_ext+N0_ext)
+                bacc_specificity += 1./N0_ext
+        
+            if pred == label0 and ext_lab[j] == label0:
+                bacc_accuracy += 1./(N1_ext+N0_ext)
+                bacc_sensitivity += 1./N1_ext
+
+    # Best Sensitivity
+    bsens_model = models[np.argmax(fold_sensitivity)]
+    bsens_accuracy = 0.
+    bsens_sensitivity = 0.
+    bsens_specificity = 0.
+
+    if ext_lab != None:
+        bsens_predict = bsens_model.predict(ext_patt)
+
+        for j, pred in enumerate(bsens_predict):
+            if pred == label1 and ext_lab[j] == label1:
+                bsens_accuracy += 1./(N1_ext+N0_ext)
+                bsens_specificity += 1./N0_ext
+        
+            if pred == label0 and ext_lab[j] == label0:
+                bsens_accuracy += 1./(N1_ext+N0_ext)
+                bsens_sensitivity += 1./N1_ext
+
+    # Best Specificity
+    bspec_model = models[np.argmax(fold_specificity)]
+    bspec_accuracy = 0.
+    bspec_sensitivity = 0.
+    bspec_specificity = 0.
+    
+    if ext_lab != None:
+        bspec_predict = bspec_model.predict(ext_patt)
+
+        for j, pred in enumerate(bspec_predict):
+            if pred == label1 and ext_lab[j] == label1:
+                bspec_accuracy += 1./(N1_ext+N0_ext)
+                bspec_specificity += 1./N0_ext
+        
+            if pred == label0 and ext_lab[j] == label0:
+                bspec_accuracy += 1./(N1_ext+N0_ext)
+                bspec_sensitivity += 1./N1_ext
+
+    # External test: Majority vote
     ext_accuracy = 0.
     ext_sensitivity = 0.
     ext_specificity = 0.
+    conf_mat = np.zeros((2,2))
 
     if ext_lab != None:
         for i, true_label in enumerate(ext_lab):
-            if vote_0_ext[i]>=vote_1_ext[i] and true_label == label0:
-                ext_accuracy += 1./(N1_ext+N0_ext)
-                ext_specificity += 1./N0_ext
-            if vote_0_ext[i]<vote_1_ext[i] and true_label == label1:
-                ext_accuracy += 1./(N1_ext+N0_ext)
-                ext_sensitivity += 1./N1_ext
+            if vote_0_ext[i]>=vote_1_ext[i]:
+                if true_label == label1:
+                    conf_mat[1][0] += 1
+                if true_label == label0:
+                    ext_accuracy += 1./(N1_ext+N0_ext)
+                    ext_specificity += 1./N0_ext
+                    conf_mat[1][1] += 1
+            if vote_0_ext[i]<vote_1_ext[i]:
+                if true_label == label1:
+                    ext_accuracy += 1./(N1_ext+N0_ext)
+                    ext_sensitivity += 1./N1_ext
+                    conf_mat[0][0] += 1
+                if true_label == label0:
+                    conf_mat[0][1] += 1
 
     res = {
         'n trees': n_trees,
@@ -115,7 +185,18 @@ def RF_binary_kfold(n_trees, k, patterns, labels, label0, label1, ext_patt = Non
         'Spec Err': np.std(fold_specificity)/np.sqrt(k),
         'Ext Acc': ext_accuracy,
         'Ext Sens': ext_sensitivity,
-        'Ext Spec': ext_specificity
+        'Ext Spec': ext_specificity,
+        'BAcc Acc': bacc_accuracy,
+        'BAcc Sens': bacc_sensitivity,
+        'BAcc Spec': bacc_specificity,
+        'BSens Acc': bsens_accuracy,
+        'BSens Sens': bsens_sensitivity,
+        'BSens Spec': bsens_specificity,
+        'BSpec Acc': bspec_accuracy,
+        'BSpec Sens': bspec_sensitivity,
+        'BSpec Spec': bspec_specificity,
+        'Models': models,
+        'Conf Mat': conf_mat
     }
 
     return res
@@ -153,18 +234,19 @@ def RF_binary_scanner(tree_range, k_range, patterns, labels, label0, label1, ext
 
     print("Finished Scanning!                                                \n")
 
-    return accuracy_list, accuracy_err_list, sensitivity_list, sensitivity_err_list, specificity_list, specificity_err_list
+    res = {
+        'Acc List': accuracy_list,
+        'Acc Err List': accuracy_err_list,
+        'Sens List': sensitivity_list,
+        'Sens Err List': sensitivity_err_list,
+        'Spec List': specificity_list,
+        'Spec Err List': specificity_err_list
+    }
 
+    return res
 
 def confMat_binary_plot(conf_mat, accuracy=None, sensitivity=None, specificity=None, precision=None, title=None):
-    root = tk.Tk()
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    root.destroy()
-
-    cmap = plt.get_cmap('bwr')
-    norm = mcolors.Normalize(vmin=np.min(conf_mat),vmax=np.max(conf_mat))
-    fig, ax = plt.subplots(figsize=(screen_width / 100, screen_height / 100))
+    fig, ax = plt.subplots()
     if title == None:
         ax.set_title("Confusion matrix")
     else:
@@ -183,6 +265,19 @@ def confMat_binary_plot(conf_mat, accuracy=None, sensitivity=None, specificity=N
         accuracy = (conf_mat[0][0] + conf_mat[1][1]) / tot
     F1score = 2. * (precision * sensitivity) / (precision + sensitivity)
 
+    norm = colors.Normalize(vmin = 0, vmax = np.max(conf_mat))
+    normalized = norm((0., conf_mat[0][0], conf_mat[0][1], conf_mat[1][0], conf_mat[1][1]))
+    cell_color = cm.viridis(normalized)
+    middle = np.max(conf_mat)/2
+    col_max = np.argmax(conf_mat) + 1
+    text_color = np.empty((2,2,4))
+    for i in (0,1):
+        for j in (0,1):
+            if conf_mat[i][j] < middle:
+                text_color[i][j] = cell_color[col_max]
+            else:
+                text_color[i][j] = cell_color[0]
+                
     table = tab.Table(ax, loc='upper center')
     table.auto_set_font_size(False)
     fontproperties = {'family': 'sans-serif',
@@ -199,23 +294,27 @@ def confMat_binary_plot(conf_mat, accuracy=None, sensitivity=None, specificity=N
     table[0,3].visible_edges = 'BTR'
 
     table.add_cell(1,1, width = 0.2, height = 0.2, text='Total\n\n'+str(tot), loc='center', fontproperties = fontproperties)
-    table.add_cell(1,2, width = 0.3, height = 0.2, text='Positive\n\n'+str(round(conf_mat[0][0]+conf_mat[1][0],2)), loc='center', fontproperties = fontproperties)
-    table.add_cell(1,3, width = 0.3, height = 0.2, text='Negative\n\n'+str(round(conf_mat[0][1]+conf_mat[1][1],2)), loc='center', fontproperties = fontproperties)
+    table.add_cell(1,2, width = 0.3, height = 0.2, text='Positive\n\n'+str(conf_mat[0][0]+conf_mat[1][0]), loc='center', fontproperties = fontproperties)
+    table.add_cell(1,3, width = 0.3, height = 0.2, text='Negative\n\n'+str(conf_mat[0][1]+conf_mat[1][1]), loc='center', fontproperties = fontproperties)
 
     table.add_cell(2,0, width = 0.1, height = 0.3, text='Classifier', loc='center', fontproperties = fontproperties)
     table[2,0].set_text_props(rotation = 'vertical')
     table[2,0].visible_edges = 'LTR'
-    table.add_cell(2,1, width = 0.2, height = 0.3, text='Positive\n\n'+str(conf_mat[0][0]+conf_mat[0][1]), loc='center', fontproperties = fontproperties)
-    table.add_cell(2,2, width = 0.3, height = 0.3, text=str(round(conf_mat[0][0],2)), loc='center', fontproperties = fontproperties, facecolor='white')
-    table.add_cell(2,3, width = 0.3, height = 0.3, text=str(round(conf_mat[0][1],2)), loc='center', fontproperties = fontproperties, facecolor='white')
+    table.add_cell(2,1, width = 0.2, height = 0.3, text='Positive\n\n'+str(round(conf_mat[0][0]+conf_mat[0][1], 2)), loc='center', fontproperties = fontproperties)
+    table.add_cell(2,2, width = 0.3, height = 0.3, text=str(conf_mat[0][0]), loc='center', fontproperties = fontproperties, facecolor = cell_color[1])
+    table[2,2].set_text_props(c = text_color[0][0])
+    table.add_cell(2,3, width = 0.3, height = 0.3, text=str(conf_mat[0][1]), loc='center', fontproperties = fontproperties, facecolor = cell_color[2])
+    table[2,3].set_text_props(c = text_color[0][1])
     table.add_cell(2,4, width = 0.2, height = 0.3, text='Precision\n\n{:.2}'.format(precision), loc='center', fontproperties = fontproperties)
 
     table.add_cell(3,0, width = 0.1, height = 0.3, text='Output of', loc='center', fontproperties = fontproperties)
     table[3,0].set_text_props(rotation = 'vertical')
     table[3,0].visible_edges = 'BLR'
-    table.add_cell(3,1, width = 0.2, height = 0.3, text='Negative\n\n'+str(conf_mat[1][0]+conf_mat[1][1]), loc='center', fontproperties = fontproperties)
-    table.add_cell(3,2, width = 0.3, height = 0.3, text=str(conf_mat[1][0]), loc='center', fontproperties = fontproperties, facecolor='white')
-    table.add_cell(3,3, width = 0.3, height = 0.3, text=str(conf_mat[1][1]), loc='center', fontproperties = fontproperties, facecolor='white')
+    table.add_cell(3,1, width = 0.2, height = 0.3, text='Negative\n\n'+str(round(conf_mat[1][0]+conf_mat[1][1], 2)), loc='center', fontproperties = fontproperties)
+    table.add_cell(3,2, width = 0.3, height = 0.3, text=str(conf_mat[1][0]), loc='center', fontproperties = fontproperties, facecolor = cell_color[3])
+    table[3,2].set_text_props(c = text_color[1][0])
+    table.add_cell(3,3, width = 0.3, height = 0.3, text=str(conf_mat[1][1]), loc='center', fontproperties = fontproperties, facecolor = cell_color[4])
+    table[3,3].set_text_props(c = text_color[1][1])
     table.add_cell(3,4, width = 0.2, height = 0.3)
 
     table.add_cell(4,2, width = 0.3, height = 0.2, text='Sensitivity\n\n{:.2}'.format(sensitivity), loc='center', fontproperties = fontproperties)
